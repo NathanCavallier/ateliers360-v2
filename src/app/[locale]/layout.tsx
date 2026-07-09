41fcc5bb2f8c80cfa3380a121a14a970f92c19eb
imogonathan-ops <imogonathan@github.com>
2026-07-09 12:50:07 +0200

    feat: Mettre à jour les références d'Ateliers 360 Lab à Ateliers 360 dans les fichiers JSON et TypeScript pour assurer la cohérence de la marque

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -20,18 +20,18 @@ type Props = {
 const locales = ["en", "fr"];
 
 export const metadata: Metadata = {
-  title: "Ateliers 360 Lab",
+  title: "Ateliers 360",
   description: "Ateliers Tech et Sciences",
   openGraph: {
-    title: "Ateliers 360 Lab",
+    title: "Ateliers 360",
     description: "Ateliers Tech et Sciences",
     url: "https://www.ateliers360.fr",
-    siteName: "Ateliers 360 Lab",
+    siteName: "Ateliers 360",
     images: [
       {
         url:
           "https://orzfuxasrbpkcaqvgvah.supabase.co/storage/v1/object/sign/images/logo_Ateliers360.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtl eV80MzVkYjM4Ni1kN2Q5LTQwZWEtYmE5Mi04MTMwOTRhZjg2YTUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJpbWFnZXMvbG9nb19BdGVsaWVyczM2MC5wbmciLCJpYXQiOjE3NzgwMDA3NTYsImV4cCI6MTkzNTY4MDc1Nn0.-cSRdRkuxaaoNV3BCP8-aWNWb4ZGss_JM1_tB1LrSXA", // Lien direct vers l'image dans Supabase Storage (assurez-vous que les permissions sont correctement configurées)
-        alt: "Ateliers 360 Lab Logo",
+        alt: "Ateliers 360 Logo",
       },
     ],
     locale: "fr_FR",
